1ce7ceebfd2fada47605f039cd78043748b0238e
ian5030560 <110732376+ian5030560@users.noreply.github.com>
2023-11-10 15:50:52 +0800

    compose

--- simple-note-2/front/simple-note-2/src/App.jsx
+++ simple-note-2/front/simple-note-2/src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-<<<<<<< Updated upstream
 import Editor from "./editor/editor"
 import Tree from './tree/tree'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
@@ -7,20 +6,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 export default function App() {
     return (<BrowserRouter>
         <Routes>
-
             <Route index element={<Editor />} />
             <Route path='tree' element={<Tree />} />
-            {/* <Route path='dashboard' element={<Tool />} /> */}
-   
         </Routes>
     </BrowserRouter>)
-=======
-import Toolbar from "./Toolbar"
-// import Tree from './tree/tree'
-
-export default function App() {
-    return (
-    <Toolbar/>
-    );
->>>>>>> Stashed changes
 }
