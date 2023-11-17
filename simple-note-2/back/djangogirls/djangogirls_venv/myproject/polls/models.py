f163926c9881acaa30bb0187e69a7a9c6d4f14cb
2**10 <116153038+itsukif6@users.noreply.github.com>
2023-11-17 16:59:16 +0800

    signin

--- simple-note-2/back/djangogirls/djangogirls_venv/myproject/polls/models.py
+++
@@ -1,7 +0,0 @@
-# models.py
-from django.db import models
-
-
-class YourModel(models.Model):
-    your_field = models.CharField(max_length=100)
-    # 添加其他需要的字段
--- simple-note-2/back/djangogirls/djangogirls_venv/myproject/polls/urls.py
+++
@@ -1,8 +0,0 @@
-# urls.py
-from django.urls import path
-from .views import receive_data
-
-urlpatterns = [
-    # 其他路径...
-    path("receive_data/", receive_data, name="receive_data"),
-]
