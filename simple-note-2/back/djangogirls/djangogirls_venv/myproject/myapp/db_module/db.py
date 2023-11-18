2242d58771efa9fb1d368d1fafb0a4fce0f94fde
ian5030560 <110732376+ian5030560@users.noreply.github.com>
2023-11-18 20:54:51 +0800

    post the detail comment of DB module

--- simple-note-2/back/djangogirls/djangogirls_venv/myproject/myapp/db_module/db.py
+++ simple-note-2/back/djangogirls/djangogirls_venv/myproject/myapp/db_module/db.py
@@ -7,17 +7,25 @@ class DB:
 
     """
     Property:\n
-    \tsettings (dict): {
-    \t    "host": your host,
-    \t    "port": your port,
-    \t    "user": your username,
-    \t    "password": your password,
-    \t    "db": your database
-    \t}\n
     \ttableName (str): your table name
+    Example:\n
+    \t(1)\n 
+    \tDB.tableName = your table name\n
+    \tDB.delete("col1 = 1")\n
+    \tDB.insert([("abc123", 100000)])\n
+    \tDB.update("salary = 100000", "col1 = 2")\n
+    \tDB.query("SELECT * FROM your_table")\n
+    \t############################################\n
+    \t(2)\n
+    \tclass MyTable(DB):\n
+    \t\ttableName = your table name\n
+    \tMyTable.delete("col1 = 1")\n
+    \tMyTable.insert([("abc123", 100000)])\n
+    \tMyTable.update("salary = 100000", "col1 = 2")\n
+    \tMyTable.query("SELECT * FROM your_table")\n
     """
 
-    tableName = "user"
+    tableName = ""
 
     settings = {
         "host": "sql12.freesqldatabase.com",
@@ -50,18 +58,31 @@ class DB:
             return False
 
     @classmethod
-    def delete(cls, condition: str = "TRUE", table=None) -> bool:
-        if not table:
-            table = cls.tableName
+    def delete(cls, condition: str = "TRUE") -> bool:
+        """
+        delete data from table with sql condition\n
+        Args:\n
+            condition (str, optional): sql condition, Defaults to "TRUE".
 
+        Returns:
+            bool: True if successful else False
+        """
         def innerDelete(cursor: cursors.Cursor):
-            cursor.execute("DELETE FROM `{}` WHERE {};".format(table, condition))
+            cursor.execute("DELETE FROM `{}` WHERE {};".format(cls.tableName, condition))
             cls.__conn.commit()
 
         return cls.__execute(innerDelete)
 
     @classmethod
     def query(cls, command: str) -> "tuple | None":
+        """
+        access to table with sql sentence\n
+        Args:\n
+            command (str): sql sentence
+
+        Returns:
+            tuple | None
+        """
         data: tuple = None
 
         def innerQuery(cursor: cursors.Cursor):
@@ -76,24 +97,35 @@ class DB:
         return data
 
     @classmethod
-    def insert(cls, data: list, table: str = None) -> bool:
-        if not table:
-            table = cls.tableName
+    def insert(cls, data: list[tuple]) -> bool:
+        """
+        insert a list of data into the table\n
+        Args:
+            data (list[tuple]): a list of data
 
+        Returns:
+            bool: True if successful else False
+        """
         def innerInsert(cursor: cursors.Cursor):
-            cursor.execute("INSERT INTO `{}` VALUES {}".format(table, *data))
+            cursor.execute("INSERT INTO `{}` VALUES {}".format(cls.tableName, *data))
             cls.__conn.commit()
 
         return cls.__execute(innerInsert)
 
     @classmethod
-    def update(cls, setting: str, condition: str = "TRUE", table=None) -> bool:
-        if not table:
-            table = cls.tableName
+    def update(cls, setting: str, condition: str = "TRUE") -> bool:
+        """
+        update data in the table with the given condition and settings
+        Args:
+            setting (str): sql sentence\n
+            condition (str, optional): sql condition, Defaults to "TRUE".
 
+        Returns:
+            bool: True if successful else False
+        """
         def innerUpdate(cursor: cursors.Cursor):
             cursor.execute(
-                "UPDATE `{}` SET {} WHERE {}".format(table, setting, condition)
+                "UPDATE `{}` SET {} WHERE {}".format(cls.tableName, setting, condition)
             )
             cls.__conn.commit()
 
@@ -101,11 +133,16 @@ class DB:
 
     @classmethod
     def close(cls) -> bool:
+        """
+        close the connection with database\n
+        if not connected yet, it returns True by default\n
+        Returns:
+            bool: True if successful else False
+        """
         if not cls.__conn:
             return True
 
         try:
-            cls.__conn.cursor
             cls.__conn.close()
             return True
 
@@ -113,8 +150,11 @@ class DB:
             return False
 
 
-# class UserDB(DB):
-#     tableName = "user"
-
-# print(UserDB.delete())
-# UserDB.close()
+class UserTable(DB):
+    """
+    Table: `user`\n
+    Columns:\n
+    \tuser_acct(str, primary): user account, length <= 320\n
+    \tuser_pwd(str): user password, length <= 30\n
+    """
+    tableName = "user"
