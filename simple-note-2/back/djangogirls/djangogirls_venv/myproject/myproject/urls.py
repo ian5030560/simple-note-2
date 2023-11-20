4050fb10a99f9c3dd753af4b979e3624383e2d6b
2**10 <116153038+itsukif6@users.noreply.github.com>
2023-11-20 10:47:07 +0800

    signin

--- simple-note-2/back/djangogirls/djangogirls_venv/myproject/myproject/urls.py
+++ simple-note-2/back/djangogirls/djangogirls_venv/myproject/myproject/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.urls import re_path
+from django.urls import path, include, re_path, include
 from myapp.views import *
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", ReactView.as_view(), name="anything"),
-]
+    path("signin/", include("signin.urls")),
+    path("", RedirectView.as_view(url="/signin/", permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
