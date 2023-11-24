83bc4d66e7fba9950ce6d72998040f9bd42923b0
limerence0z3 <godbottle98@gmail.com>
2023-11-24 15:45:03 +0800

    0

--- simple-note-2/back/djangogirls/djangogirls_venv/myproject/db_modules/db.py
+++ simple-note-2/back/djangogirls/djangogirls_venv/myproject/db_modules/db.py
@@ -8,7 +8,7 @@ conn = sqlite3.connect(dbfile)
 
 
 class DB:
-    
+    #check_signin
     def check_signin(self, username, user_password):
         # 使用 SQL 查詢檢查是否有相同的使用者名稱和密碼
         cursor = conn.cursor()
@@ -18,7 +18,8 @@ class DB:
         # 如果有相同的資料，回傳 True；否則回傳 False
         return bool(row)
 
-    def check_register(self, username):
+    #check_register
+    def check_register_username(self, username):
         # 使用 SQL 查詢檢查是否有相同的使用者名稱
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM users WHERE username = ?;", (username,))
@@ -27,7 +28,7 @@ class DB:
         # 如果有相同的資料，回傳 True；否則回傳 False
         return bool(row)
 
-    def check_register_email(self, user_email):
+    def check_register_user_email(self, user_email):
         # 使用 SQL 查詢檢查是否有相同的使用者電子郵件
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM users WHERE user_email = ?;", (user_email,))
@@ -35,6 +36,7 @@ class DB:
         row = cursor.fetchone()
         # 如果有相同的資料，回傳 True；否則回傳 False
         return bool(row)                
+    
         
     def fetch_user_register_data(self):
         # 查詢並列印 User_Register_Data 表格的資料
