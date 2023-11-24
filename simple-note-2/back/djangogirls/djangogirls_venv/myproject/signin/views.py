166f40072fc510a6bc28fa368f6adb97e8b08510
2**10 <116153038+itsukif6@users.noreply.github.com>
2023-11-24 17:08:28 +0800

    back

--- simple-note-2/back/djangogirls/djangogirls_venv/myproject/signin/views.py
+++ simple-note-2/back/djangogirls/djangogirls_venv/myproject/signin/views.py
@@ -19,14 +19,35 @@ class SigninView:
 
     def post(self, request, format=None):
         try:
-            account = request.POST["account"]
+            email = request.POST["email"]
             password = request.POST["password"]
             username = request.POST["username"]
             id = request.POST["id"]
         except KeyError:
-            account = None
+            email = None
             password = None
             username = None
             id = None
-        print(account, password, username, id)
-        return Response(status=status.HTTP_200_OK)
+        if id == "sign-in":
+            if DB.check_signin(username, password) == True:
+                return Response(status=status.HTTP_200_OK)  # 登入成功
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)  # 登入失敗
+        elif id == "register":
+            check_username = DB.check_register_username(username)
+            check_email = DB.check_register_user_email(email)
+
+            if check_username == True:
+                return Response(status=status.HTTP_401_UNAUTHORIZED)  # username重複，不能註冊
+
+            elif check_username == False:  # username不重複
+                if check_email == True:
+                    return Response(
+                        status=status.HTTP_402_PAYMENT_REQUIRED
+                    )  # email重複，不能註冊
+
+                elif check_email == False:
+                    return Response(status=status.HTTP_201_CREATED)  # email不重複，可以註冊
+
+        print(email, password, username, id)
+        return Response(status=status.HTTP_404_NOT_FOUND)
