34dc2d0a14cc2e013945470a22a203282a76f6b3
ian5030560 <110732376+ian5030560@users.noreply.github.com>
2023-11-24 18:51:20 +0800

    Update basic.jsx

--- simple-note-2/front/simple-note-2/src/auth/basic.jsx
+++ simple-note-2/front/simple-note-2/src/auth/basic.jsx
@@ -32,6 +32,7 @@ function postData(url, path, data, onReceive, onError) {
             url: url,
             mode: "cors",
             method: "POST",
+            credentials: "include",
             headers: {
                 "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
                 "content-type": "application/json",
