b79c4d6bad701111fd6cc635912431e9a4c0c413
ian5030560 <110732376+ian5030560@users.noreply.github.com>
2023-12-01 13:37:30 +0800

    update

--- simple-note-2/front/simple-note-2/src/user/component/menu.jsx
+++ simple-note-2/front/simple-note-2/src/user/component/menu.jsx
@@ -5,7 +5,7 @@ const { Text } = Typography;
 
 const ToolLine = ({ nodeKey, onDelete, onAdd }) => {
 
-    return <Flex style={{color: "white"}}>
+    return <Flex style={{ color: "white" }}>
         <DeleteOutlined
             onClick={(e) => {
                 e.stopPropagation();;
@@ -23,7 +23,7 @@ const ToolLine = ({ nodeKey, onDelete, onAdd }) => {
 const Node = ({ text, nodeKey, onAdd, onDelete }) => {
 
     return <Flex gap={"large"}>
-        <Text ellipsis style={{color: "white"}}>{text}</Text>
+        <Text ellipsis style={{ color: "white" }}>{text}</Text>
         <ToolLine
             nodeKey={nodeKey}
             onAdd={onAdd}
@@ -63,32 +63,84 @@ function getParentKey(key) {
     return result;
 }
 
+// eslint-disable-next-line no-unused-vars
+const dataType = {
+    key: "",
+    title: "",
+    /**
+     * @type {Array}
+     */
+    children: [],
+    name: "",
+}
 /**
  * 
- * @param {{data: Array}} param0 
+ * @param {{i_data: Array<dataType>, m_data: Array<dataType>}} param0 
  * @returns 
  */
-const FileMenu = ({ i_data, m_data }) => {
+const FileMenu = ({ 
+    i_data, 
+    m_data, 
+    onSelect, 
+    onIndiviualAdd, 
+    onIndiviualDelete,
+    onMultipleAdd,
+    onMultipleDelete
+}) => {
 
     const { token } = theme.useToken();
-    const [i_children, setI_Children] = useState(i_data ? i_data : []);
-    const [m_children, setM_Children] = useState(m_data ? m_data : []);
+    const [i_children, setI_Children] = useState(!i_data ? [] :
+        i_data.map(data => {
+            return {
+                key: data.key,
+                title: <Node
+                    text={data.title}
+                    nodeKey={data.key}
+                    onAdd={(k) => handleAdd(k, setI_Children, onIndiviualAdd, onIndiviualDelete)}
+                    onDelete={(k) => handleDelete(k, setI_Children, onIndiviualDelete, onMultipleAdd)}
+                />,
+                children: data.children,
+                name: data.name
+            }
+        })
+    );
 
-    const handleAdd = (nodeKey, setMethod) => {
+    const [m_children, setM_Children] = useState(!m_data ? [] :
+        m_data.map(data => {
+            return {
+                key: data.key,
+                title: <Node
+                    text={data.title}
+                    nodeKey={data.key}
+                    onAdd={(k) => handleAdd(k, setM_Children, onMultipleAdd, onMultipleDelete)}
+                    onDelete={(k) => handleDelete(k, setM_Children, onMultipleDelete, onMultipleAdd)}
+                />,
+                children: data.children,
+                name: data.name
+            }
+        })
+    );
 
-        setMethod(prev => {
+    const handleAdd = (nodeKey, setChildren, onAdd, onDelete) => {
+
+        const input = onAdd?.();
+
+        if(!input) return;
+
+        setChildren(prev => {
             let target = findTargetByKey(nodeKey, prev);
 
             let key = `${nodeKey}-${target.length}`;
             target.push({
                 key: key,
                 title: <Node
-                    text={key}
+                    text={input}
                     nodeKey={key}
-                    onAdd={(k) => handleAdd(k, setMethod)}
-                    onDelete={(k) => handleDelete(k, setMethod)}
+                    onAdd={(k) => handleAdd(k, setChildren, onAdd, onDelete)}
+                    onDelete={(k) => handleDelete(k, setChildren, onDelete, onAdd)}
                 />,
-                children: []
+                children: [],
+                name: input
             })
 
             return [...prev]
@@ -99,26 +151,28 @@ const FileMenu = ({ i_data, m_data }) => {
      * 
      * @param {string} nodeKey 
      */
-    const handleDelete = (nodeKey, setMethod) => {
+    const handleDelete = (nodeKey, setChildren, onDelete, onAdd) => {
 
-        setMethod(prev => {
-            
+        onDelete?.();
+
+        setChildren(prev => {
+
             let parent = getParentKey(nodeKey);
             let target = findTargetByKey(parent, prev);
             let i = parseInt(nodeKey.charAt(nodeKey.length - 1));
             target.splice(i, 1);
 
             function changeSubtreeKey(t, p) {
-                
+
                 for (let index in t) {
                     let key = `${p}-${index}`;
 
                     t[index].key = key;
                     t[index].title = <Node
-                        text={key}
+                        text={t[index].name}
                         nodeKey={key}
-                        onAdd={(k) => handleAdd(k, setMethod)}
-                        onDelete={(k) => handleDelete(k, setMethod)}
+                        onAdd={(k) => handleAdd(k, setChildren, onAdd, onDelete)}
+                        onDelete={(k) => handleDelete(k, setChildren, onDelete, onAdd)}
                     />
 
                     changeSubtreeKey(t[index].children, key);
@@ -137,8 +191,8 @@ const FileMenu = ({ i_data, m_data }) => {
             title: <Node
                 text={"個人筆記"}
                 nodeKey={"individual"}
-                onAdd={(key) => handleAdd(key, setI_Children)}
-                onDelete={(key) => handleDelete(key, setI_Children)}
+                onAdd={(key) => handleAdd(key, setI_Children, onIndiviualAdd, onIndiviualDelete)}
+                onDelete={(key) => handleDelete(key, setI_Children, onIndiviualDelete, onIndiviualAdd)}
             />,
             children: i_children
         },
@@ -147,8 +201,8 @@ const FileMenu = ({ i_data, m_data }) => {
             title: <Node
                 text={"多人協作"}
                 nodeKey={"multiple"}
-                onAdd={(key) => handleAdd(key, setM_Children)}
-                onDelete={(key) => handleDelete(key, setM_Children)}
+                onAdd={(key) => handleAdd(key, setM_Children, onMultipleAdd, onMultipleDelete)}
+                onDelete={(key) => handleDelete(key, setM_Children, onMultipleDelete, onMultipleAdd)}
             />,
             children: m_children
         }
@@ -156,11 +210,11 @@ const FileMenu = ({ i_data, m_data }) => {
 
     return <Tree
         treeData={rootData}
-        rootStyle={{ 
+        rootStyle={{
             backgroundColor: token.colorPrimary,
             color: "white"
         }}
-        // onSelect={(key) => console.log(key)}
+        onSelect={onSelect}
     />
 }
 
