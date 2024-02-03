d6064a18afad5532ce5d5b4b4005d60aaeebd39d
ian5030560 <tom5030560@gmail.com>
2024-02-04 01:28:49 +0800

    delete previous js project

--- simple-note-2/front/simple-note-2/src/editor/slate/Component/image/add.js
+++
@@ -1,5 +0,0 @@
-import createAddItem from "../../spec/add";
-
-const image = createAddItem("image", "Image");
-
-export default image;
--- simple-note-2/front/simple-note-2/src/editor/slate/spec/plugin.js
+++
@@ -1,9 +0,0 @@
-import { Editor } from "slate";
-/**
- * 
- * @param {(editor: Editor) => Editor} plugin 
- */
-export default function createPlugin(plugin){
-    
-    return plugin;
-}
