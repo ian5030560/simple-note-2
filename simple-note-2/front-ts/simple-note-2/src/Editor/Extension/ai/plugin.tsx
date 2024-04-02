28c33600f339b90cda606cd10f9e8224b37a524a
ian5030560 <110732376+ian5030560@users.noreply.github.com>
2024-04-02 17:57:05 +0800

    update

--- simple-note-2/front-ts/simple-note-2/src/Editor/Extension/ai/plugin.tsx
+++ simple-note-2/front-ts/simple-note-2/src/Editor/Extension/ai/plugin.tsx
@@ -2,9 +2,10 @@ import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext
 import { Plugin } from "..";
 import { useCallback, useEffect, useState } from "react";
 import { mergeRegister } from "@lexical/utils";
-import { $getNodeByKey, $getSelection, $isRangeSelection, $isTextNode, KEY_TAB_COMMAND, SELECTION_CHANGE_COMMAND, TextNode} from "lexical";
+import { $getNodeByKey, $getSelection, $isRangeSelection, $isTextNode, KEY_TAB_COMMAND, LexicalCommand, SELECTION_CHANGE_COMMAND, TextNode, createCommand} from "lexical";
 import "./plugin.css";
 
+const CTRL_SHIFT: LexicalCommand<void> = createCommand();
 export const AIPlaceholderPlugin: Plugin = () => {
     const [editor] = useLexicalComposerContext();
     const [key, setKey] = useState("");
@@ -16,6 +17,16 @@ export const AIPlaceholderPlugin: Plugin = () => {
         element?.setAttribute("data-text", "");
         
     }, [editor, key]);
+
+    useEffect(() => {
+        let root = editor.getRootElement();
+
+        function handleCtrlShift(e: KeyboardEvent){
+
+        }
+        root?.addEventListener("keyup", handleCtrlShift);
+        return () => root?.removeEventListener("keyup", handleCtrlShift);
+    }, []);
 
     useEffect(() => {
         return mergeRegister(
@@ -32,7 +43,7 @@ export const AIPlaceholderPlugin: Plugin = () => {
                 const element = editor.getElementByKey(key);
                 element?.setAttribute("data-text", text);
             }),
-
+            
             editor.registerCommand(SELECTION_CHANGE_COMMAND, () => {
                 const selection = $getSelection();
                 let key = "";
--- simple-note-2/front-ts/simple-note-2/src/Editor/Extension/index.ts
+++ simple-note-2/front-ts/simple-note-2/src/Editor/Extension/index.ts
@@ -36,7 +36,7 @@ const Extensions: Extension[] = [
     ImageToTextExtension,
     VideoExtension,
     DocumentExtension,
-    // AIExtension,
+    AIExtension,
     ColumnLayoutExtension,
 ]
 
