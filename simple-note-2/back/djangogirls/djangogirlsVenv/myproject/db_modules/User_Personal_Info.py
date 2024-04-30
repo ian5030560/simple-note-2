a56b17f74373ff5194d72a4a691ce257bc74257e
limerence0z3 <godbottle98@gmail.com>
2024-04-30 22:31:15 +0800

    db
    搬運舊的方法中

--- simple-note-2/back/djangogirls/djangogirlsVenv/myproject/db_modules/User_Personal_Info.py
+++ simple-note-2/back/djangogirls/djangogirlsVenv/myproject/db_modules/User_Personal_Info.py
@@ -1,12 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column
-from sqlalchemy import Integer, String, DATETIME, TEXT ,BLOB,BOOLEAN
+from sqlalchemy import Integer, String, DATETIME, TEXT, BLOB, BOOLEAN
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 engine_url = "mysql+pymysql://root:w83dk4xup6@localhost:3306/simplenote2db"
 engine = create_engine(engine_url, echo=True)
+
 
 class User_Personal_Info(Base):
     __tablename__ = "User_Personal_Info"
@@ -17,8 +18,17 @@ class User_Personal_Info(Base):
     user_email = Column(String(256))
     user_password = Column(String(256))
     login_status = Column(BOOLEAN)
-    
-    def __init__(self,id,profile_photo,theme_id,usernames,user_email,user_password,login_status):
+
+    def __init__(
+        self,
+        id=None,
+        profile_photo=None,
+        theme_id=None,
+        usernames=None,
+        user_email=None,
+        user_password=None,
+        login_status=False,
+    ):
         self.id = id
         self.profile_photo = profile_photo
         self.theme_id = theme_id
@@ -26,38 +36,85 @@ class User_Personal_Info(Base):
         self.user_email = user_email
         self.user_password = user_password
         self.login_status = login_status
-    
+
 
 def create_session():
     Session = sessionmaker(bind=engine)
     session = Session()
     return session
 
+
 session = create_session()
 
-#檢查此組使用者名稱和密碼是否存在
+
+# 檢查此組使用者名稱和密碼是否存在
 def check_username_password(username, password):
-    user = session.query(User_Personal_Info).filter_by(usernames=username, user_password=password).first()
+    user = (
+        session.query(User_Personal_Info)
+        .filter_by(usernames=username, user_password=password)
+        .first()
+    )
     if user:
         return True
     else:
         return False
-    
-#檢查是否有相同的使用者名稱
+
+
+# 檢查是否有相同的使用者名稱
 def check_username(username):
     result = session.query(User_Personal_Info).filter_by(usernames=username).first()
     if result:
         return True
     else:
         return False
-#檢查是否有相同的email
+
+
+# 檢查是否有相同的email
 def check_email(user_email):
     result = session.query(User_Personal_Info).filter_by(user_email=user_email).first()
     if result:
         return True
     else:
         return False
-# #插入username,password,user_email到資料庫
-# def insert_username_password_email(username,password):             
 
-print(check_email("user01@gmail.com"))       
+#給username檢查login_status
+def check_status(username):
+    result = session.query(User_Personal_Info).filter_by(usernames=username).first()
+    if result:
+        return result.login_status
+    else:
+        return None
+
+#給user_email查password
+def search_password(email):
+    result = session.query(User_Personal_Info).filter_by(user_email=email).first()
+    if result:
+        return result.user_password
+    else:
+        return None
+                
+
+
+# 插入username,password,user_email到資料庫
+def insert_username_password_email(username, password, email):
+    new_user = User_Personal_Info(usernames=username, user_password=password, user_email=email, login_status=True)
+    session.add(new_user)
+    session.commit()
+
+#給username去change 
+def change_login_status(username):
+    user = session.query(User_Personal_Info).filter_by(usernames=username).first()
+    if user:
+        # 更新login_status True改Fasle,Fasle改True
+        if user.login_status == True:
+            user.login_status = False
+        else:
+            user.login_status = True    
+            
+        session.commit()
+        return user.login_status  
+    else:
+        return None 
+
+
+print(change_login_status("user02"))
--- simple-note-2/back/djangogirls/djangogirlsVenv/myproject/db_modules/db.py
+++ simple-note-2/back/djangogirls/djangogirlsVenv/myproject/db_modules/db.py
@@ -9,81 +9,81 @@ class DB:
         self.conn = sqlite3.connect(TEST_DB)
         self.cursor = self.conn.cursor()
 
-    def check_signin(self, username, user_password):
-        # 使用 SQL 查詢檢查是否有相同的使用者名稱和密碼
-        self.cursor.execute(
-            "SELECT * FROM User_Personal_Info WHERE username = ? AND user_password = ?;",
-            (username, user_password),
-        )
-        # 檢索結果
-        row = self.cursor.fetchone()
-        # 如果有相同的資料，回傳 True；否則回傳 False
-        return bool(row)
+    # def check_signin(self, username, user_password):
+    #     # 使用 SQL 查詢檢查是否有相同的使用者名稱和密碼
+    #     self.cursor.execute(
+    #         "SELECT * FROM User_Personal_Info WHERE username = ? AND user_password = ?;",
+    #         (username, user_password),
+    #     )
+    #     # 檢索結果
+    #     row = self.cursor.fetchone()
+    #     # 如果有相同的資料，回傳 True；否則回傳 False
+    #     return bool(row)
 
-    # check_register
-    def check_register_username(self, username):
-        # 使用 SQL 查詢檢查是否有相同的使用者名稱
-        self.cursor.execute(
-            "SELECT * FROM User_Personal_Info WHERE username = ?;", (username,)
-        )
-        # 檢索結果
-        row = self.cursor.fetchone()
-        # 如果有相同的資料，回傳 True；否則回傳 False
-        return bool(row)
+    # # check_register
+    # def check_register_username(self, username):
+    #     # 使用 SQL 查詢檢查是否有相同的使用者名稱
+    #     self.cursor.execute(
+    #         "SELECT * FROM User_Personal_Info WHERE username = ?;", (username,)
+    #     )
+    #     # 檢索結果
+    #     row = self.cursor.fetchone()
+    #     # 如果有相同的資料，回傳 True；否則回傳 False
+    #     return bool(row)
 
-    def check_register_user_email(self, user_email):
-        # 使用 SQL 查詢檢查是否有相同的使用者電子郵件
-        self.cursor.execute(
-            "SELECT * FROM User_Personal_Info WHERE user_email = ?;", (user_email,)
-        )
-        # 檢索結果
-        row = self.cursor.fetchone()
-        # 如果有相同的資料，回傳 True；否則回傳 False
-        return bool(row)
+    # def check_register_user_email(self, user_email):
+    #     # 使用 SQL 查詢檢查是否有相同的使用者電子郵件
+    #     self.cursor.execute(
+    #         "SELECT * FROM User_Personal_Info WHERE user_email = ?;", (user_email,)
+    #     )
+    #     # 檢索結果
+    #     row = self.cursor.fetchone()
+    #     # 如果有相同的資料，回傳 True；否則回傳 False
+    #     return bool(row)
 
-    def insert_into_User_Register_Data(self, username, user_email, user_password):
-        try:
-            # 新增資料到 User_Personal_Info 表格
-            user_data = (username, user_email, user_password, 0)
-            self.cursor.execute(
-                "INSERT INTO User_Personal_Info (username, user_email, user_password, login_status) VALUES (?, ?, ?, ?);",
-                user_data,
-            )
-            self.conn.commit()
-            return True
-        except sqlite3.Error as e:
-            return e
+    # def insert_into_User_Register_Data(self, username, user_email, user_password):
+    #     try:
+    #         # 新增資料到 User_Personal_Info 表格
+    #         user_data = (username, user_email, user_password, 0)
+    #         self.cursor.execute(
+    #             "INSERT INTO User_Personal_Info (username, user_email, user_password, login_status) VALUES (?, ?, ?, ?);",
+    #             user_data,
+    #         )
+    #         self.conn.commit()
+    #         return True
+    #     except sqlite3.Error as e:
+    #         return e
 
-    def check_signin_status(self, username):
-        self.cursor.execute(
-            "SELECT login_status FROM User_Personal_Info WHERE username = ?;",
-            (username,),
-        )
-        # 獲取查詢結果的第一行
-        row = self.cursor.fetchone()
-        # 如果有結果，取出 login_status 的值
-        if row:
-            login_status = row[0]
-            return login_status
-        else:
-            # 如果沒有結果，返回一個None
-            return None
+    # def check_signin_status(self, username):
+    #     self.cursor.execute(
+    #         "SELECT login_status FROM User_Personal_Info WHERE username = ?;",
+    #         (username,),
+    #     )
+    #     # 獲取查詢結果的第一行
+    #     row = self.cursor.fetchone()
+    #     # 如果有結果，取出 login_status 的值
+    #     if row:
+    #         login_status = row[0]
+    #         return login_status
+    #     else:
+    #         # 如果沒有結果，返回一個None
+    #         return None
 
-    # 給useremail查password
-    def useremail_to_userpassword(self, user_email):
-        self.cursor.execute(
-            "SELECT user_password FROM User_Personal_Info WHERE user_email = ?;",
-            (user_email,),
-        )
-        # 獲取查詢結果的第一行
-        row = self.cursor.fetchone()
-        # 如果有結果，取出 user_password 的值
-        if row:
-            user_password = row[0]
-            return user_password
-        else:
-            # 如果沒有結果，返回一個None
-            return None
+    # # 給useremail查password
+    # def useremail_to_userpassword(self, user_email):
+    #     self.cursor.execute(
+    #         "SELECT user_password FROM User_Personal_Info WHERE user_email = ?;",
+    #         (user_email,),
+    #     )
+    #     # 獲取查詢結果的第一行
+    #     row = self.cursor.fetchone()
+    #     # 如果有結果，取出 user_password 的值
+    #     if row:
+    #         user_password = row[0]
+    #         return user_password
+    #     else:
+    #         # 如果沒有結果，返回一個None
+    #         return None
 
     # 給username和note_name查content_blob
     def username_note_name_return_content_blob(self, username, note_name):
@@ -117,27 +117,27 @@ class DB:
             # 如果沒有結果，返回一個 None
             return None
 
-    def change_login_status(self, username):
-        self.cursor.execute(
-            "SELECT login_status FROM User_Personal_Info WHERE username = ?;",
-            (username,),
-        )
-        # 獲取查詢結果的第一行
-        row = self.cursor.fetchone()
-        if row:
-            current_status = row[0]
-            # row 0 改為 1，1 改為 0
-            new_status = 1 if current_status == 0 else 0
-            # UPDATE更新 login_status
-            self.cursor.execute(
-                "UPDATE User_Personal_Info SET login_status = ? WHERE username = ?;",
-                (new_status, username),
-            )
-            # 提交修改
-            self.conn.commit()
-            return True
-        else:
-            return f"No result found for {username}"
+    # def change_login_status(self, username):
+    #     self.cursor.execute(
+    #         "SELECT login_status FROM User_Personal_Info WHERE username = ?;",
+    #         (username,),
+    #     )
+    #     # 獲取查詢結果的第一行
+    #     row = self.cursor.fetchone()
+    #     if row:
+    #         current_status = row[0]
+    #         # row 0 改為 1，1 改為 0
+    #         new_status = 1 if current_status == 0 else 0
+    #         # UPDATE更新 login_status
+    #         self.cursor.execute(
+    #             "UPDATE User_Personal_Info SET login_status = ? WHERE username = ?;",
+    #             (new_status, username),
+    #         )
+    #         # 提交修改
+    #         self.conn.commit()
+    #         return True
+    #     else:
+    #         return f"No result found for {username}"
 
     # 透過username和file_title_id插入內容
     def update_content_username_file_title_id(self, username, file_title_id, content):
