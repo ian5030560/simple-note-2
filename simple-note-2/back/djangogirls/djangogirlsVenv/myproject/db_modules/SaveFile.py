b407e73765116882182c2491383a663d0064693e
2**10 <aaron911024@gmail.com>
2024-06-25 15:44:47 +0800

    update insert file

---
+++ simple-note-2/back/djangogirls/djangogirlsVenv/myproject/db_modules/SaveFile.py
@@ -0,0 +1,14 @@
+﻿import os
+
+class SaveFile:
+    def __init__(self, folder_path) -> None:
+        self.folder_path = folder_path
+        if not os.path.exists(self.folder_path):
+            os.makedirs(self.folder_path)
+    
+    def saveNewFile(self, filename, content):
+        file_path = os.path.join(self.folder_path, filename)
+        with open(file_path, 'wb') as file:
+            file.write(content)
+        print(f'File saved at {file_path}')
+        return True
