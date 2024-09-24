19b26a373db00942af65f7d0f10f3a1dee342cf7
2**10 <116153038+itsukif6@users.noreply.github.com>
2024-09-24 17:50:22 +0800

    update dont look

--- simple-note-2/back/djangogirls/djangogirlsVenv/myproject/myproject/settings.py
+++ simple-note-2/back/djangogirls/djangogirlsVenv/myproject/myproject/settings.py
@@ -180,4 +180,5 @@ EMAIL_HOST = "smtp.gmail.com"  # SMTP伺服器
 EMAIL_PORT = 587  # TLS通訊埠號
 EMAIL_USE_TLS = True  # 開啟TLS(傳輸層安全性)
 EMAIL_HOST_USER = "s2mplenote.nknu@gmail.com"  # 寄件者電子郵件
-EMAIL_HOST_PASSWORD = "fkfs igrw bjxz yzkj"  # Gmail應用程式的密碼
+from . import password
+EMAIL_HOST_PASSWORD = password.EMAIL_HOST_PASSWORD  # Gmail應用程式的密碼
