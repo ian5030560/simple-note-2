fddce005e6620ca0438e946f4effa5acbf76aebf
limerence0z3 <66242223+limerence0z3@users.noreply.github.com>
2024-11-19 10:38:07 +0800

    UserPersonalThemeData
    新增查詢所有資料by給username
    新增刪除by給theme id(資料庫的id)

--- simple-note-2/back/djangogirls/djangogirlsVenv/myproject/db_modules/UserPersonalThemeData.py
+++ simple-note-2/back/djangogirls/djangogirlsVenv/myproject/db_modules/UserPersonalThemeData.py
@@ -1,8 +1,7 @@
-from sqlalchemy import and_, create_engine, insert, update
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import and_, create_engine, insert, update, delete
 from sqlalchemy import Column
 from sqlalchemy import Integer, String, DATETIME, TEXT, BLOB, BOOLEAN
-from sqlalchemy.orm import sessionmaker ,scoped_session
+from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
 from .UserPersonalInfo import User_Personal_Info
 from sqlalchemy.exc import SQLAlchemyError
 import os
@@ -31,15 +30,19 @@ class User_Personal_Theme_Data(Base):
 # def create_session():
 #     Session = sessionmaker(bind=engine)
 #     session = Session()
-    # return session
-    
+# return session
+
+
 def create_session():
     Session = scoped_session(sessionmaker(bind=engine))
     return Session
 
 
 # check_theme_name
-def check_theme_name(usernames_input, theme_name_input):
+def check_theme_name(
+    usernames_input,
+    theme_name_input,
+):
     session = create_session()
     user_id_query = (
         session.query(User_Personal_Info.id)
@@ -56,6 +59,60 @@ def check_theme_name(usernames_input, theme_name_input):
         return True
     else:
         return False
+
+
+# Return all theme data by username
+def check_all_theme_data(usernames_input):
+    session = create_session()
+    try:
+        # 查詢 user_id
+        user_id_query = (
+            session.query(User_Personal_Info.id)
+            .filter(User_Personal_Info.usernames == usernames_input)
+            .first()
+        )
+
+        # 如果 user_id_query 是 None，直接返回空列表
+        if not user_id_query:
+            return []
+
+        # 查詢與 user_id 對應的所有主題資料
+        result = (
+            session.query(
+                User_Personal_Theme_Data.id, 
+                User_Personal_Theme_Data.theme_name,
+                User_Personal_Theme_Data.color_light_primary,
+                User_Personal_Theme_Data.color_light_base_bg,
+                User_Personal_Theme_Data.color_dark_primary,
+                User_Personal_Theme_Data.color_dark_base_bg,
+            )
+            .filter(User_Personal_Theme_Data.user_id == user_id_query[0])
+            .all()
+        )
+        return result  # 返回主題資料列表
+    except SQLAlchemyError as e:
+        #print(e)
+        return False
+    finally:
+        # 確保 session 被正確關閉
+        session.close()
+
+
+# Delete one theme data by theme id(database 的 id)
+def delete_one_theme_data(theme_id_input):
+    session = create_session()
+    try:
+        stmt = delete(User_Personal_Theme_Data).where(
+            User_Personal_Theme_Data.id == theme_id_input
+        )
+        session.execute(stmt)
+        session.commit()
+        return True
+    except SQLAlchemyError as e:
+        session.rollback()
+        return str(e)
+    finally:
+        session.close()
 
 
 # update_theme_name_by_username
@@ -87,9 +144,17 @@ def update_theme_name(usernames_input, old_theme_name_input, new_theme_name_inpu
         return False
     finally:
         session.close()
-        
-#update theme data by username and theme name
-def update_themeData_by_usernames(usernames_input,theme_name_input,color_light_primary_input,color_light_base_bg_input,color_dark_primary_input,color_dark_base_bg_input):
+
+
+# update theme data by username and theme name
+def update_themeData_by_usernames(
+    usernames_input,
+    theme_name_input,
+    color_light_primary_input,
+    color_light_base_bg_input,
+    color_dark_primary_input,
+    color_dark_base_bg_input,
+):
     session = create_session()
     user_id_query = (
         session.query(User_Personal_Info.id)
@@ -121,7 +186,7 @@ def update_themeData_by_usernames(usernames_input,theme_name_input,color_light_p
         return False
     finally:
         session.close()
-    
+
 
 # insert_theme_name_by_username
 def insert_theme_name_by_username(usernames_input, theme_name_input):
@@ -191,3 +256,4 @@ def insert_themeData_by_usernames(
         session.close()
 
 
+#print(delete_one_theme_data("3"))
