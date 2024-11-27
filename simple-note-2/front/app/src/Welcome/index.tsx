21ef5aef8e71edfdca97a5eb9d9a916b25b930c1
ian5030560 <tom5030560@gmail.com>
2024-11-27 15:40:09 +0800

    Update index.tsx

--- simple-note-2/front/app/src/Welcome/index.tsx
+++ simple-note-2/front/app/src/Welcome/index.tsx
@@ -9,9 +9,9 @@ const { useBreakpoint } = Grid;
 const NoteImage: React.FC = () => <Image src={Note} alt="" width={64} height={64} preview={false} />
 
 const Brand = () => {
-    const { xl } = useBreakpoint();
+    const { lg } = useBreakpoint();
 
-    return <Flex justify={xl ? "center" : "start"} align="center" gap="small">
+    return <Flex justify={lg ? "center" : "start"} align="center" gap="small">
         <NoteImage />
         <Typography.Title level={2} ellipsis>Simple-Note-2</Typography.Title>
     </Flex>
@@ -19,13 +19,13 @@ const Brand = () => {
 
 export default function WelcomeLayout() {
     const { token } = theme.useToken();
-    const { sm } = useBreakpoint();
+    const { xxl } = useBreakpoint();
 
     return <Flex vertical style={{ height: "100%" }}>
         <Row align={"middle"} style={{ backgroundColor: token.colorBgBase, padding: "2em 1em" }}>
             <Col xs={20} sm={12}><Brand /></Col>
             <Col xs={4} sm={12}>
-                <Flex justify={sm ? "center" : "end"}><NavigateMenu /></Flex>
+                <Flex justify={xxl ? "center" : "end"}><NavigateMenu /></Flex>
             </Col>
         </Row>
         <div style={{ backgroundColor: token.colorBgBase, flexGrow: 1 }}>
