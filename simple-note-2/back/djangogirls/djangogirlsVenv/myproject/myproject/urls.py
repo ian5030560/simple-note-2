c2f8bea0ad233a957e2fb6c438820e1ac72e46e8
2**10 <116153038+itsukif6@users.noreply.github.com>
2024-12-03 11:04:08 +0800

    Update urls.py

--- simple-note-2/back/djangogirls/djangogirlsVenv/myproject/myproject/urls.py
+++ simple-note-2/back/djangogirls/djangogirlsVenv/myproject/myproject/urls.py
@@ -15,35 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-'''
-import sys
-
-# new url add path here
-sys.path.append("..deleteFile")
-sys.path.append("..deleteNote")
-sys.path.append("..forgetPassword")
-sys.path.append("..breeze")
-sys.path.append("..getInfo")
-sys.path.append("..getNote")
-sys.path.append("..logout")
-sys.path.append("..myapp")
-sys.path.append("..newMediaFile")
-sys.path.append("..newNote")
-sys.path.append("..newTheme")
-sys.path.append("..registerAndLogin")
-sys.path.append("..saveNote")
-sys.path.append("..updateInfo")
-sys.path.append("..updateMediaFile")
-sys.path.append("..viewMediaFile")
-sys.path.append("..loadNoteTree")
-sys.path.append("..newCollaborate")
-sys.path.append("..deleteCollaborate")
-sys.path.append("..joinCollaborate")
-sys.path.append("..aiSocket")
-sys.path.append("..getTheme")
-sys.path.append("..deleteTheme")
-'''
-
 import sys
 from pathlib import Path
 
@@ -131,59 +102,7 @@ schema_view = get_schema_view(
   description='',
   )
 )
-'''
-# urls
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path(
-        "registerAndLogin/", RegisterAndLoginView.as_view(), name="registerAndLogin"
-    ),  # registerAndLogin
-    path(
-        "forgetPassword/", ForgetPasswordView.as_view(), name="forgetPassword"
-    ),  # forgetPassword
-    path("logout/", LogoutView.as_view(), name="logout"),  # logout
-    path("newMediaFile/", NewMediaFileView.as_view(), name="newMedia"),  # newMedia
-    path(
-        "updateMediaFile/", UpdateMediaFileView.as_view(), name="updateMediaFile"
-    ),  # updateMediaFile
-    path("deleteFile/", DeleteFileView.as_view(), name="deleteFile"),  # deleteFile
-    path("viewMediaFile/", ViewMediaFileView.as_view(), name="viewMedia"),  # viewMedia
-    path("getInfo/", GetInfoView.as_view(), name="getInfo"),  # getInfo
-    path("updateInfo/", UpdateInfoView.as_view(), name="updateInfo"),  # updateInfo
-    path("newTheme/", NewThemeView.as_view(), name="newTheme"),  # newTheme
-    path("breeze/", BreezeView.as_view(), name="breeze"),  # breezeAI model
-    path(
-        "viewMediaFile/<username>",
-        ViewMediaFileView.as_view(),
-        name="viewMediaFile/<username>",
-    ),  # viewMediaFile/username
-    path(
-        "viewMediaFile/<username>/<notename>",
-        ViewMediaFileView.as_view(),
-        name="viewMediaFile/<username>/<notename>",
-    ),  # viewMediaFile/username/filename
-    path(
-        "viewMediaFile/<username>/<notename>/<filename>",
-        ViewMediaFileView.as_view(),
-        name="viewMediaFile/<username>/<notename>/<filename>",
-    ),  # viewMediaFile/username/notename/filename
-    path("getNote/", GetNoteView.as_view(), name="getNote"),  # getNote
-    path("newNote/", NewNoteView.as_view(), name="newNote"),  # newNote
-    path("deleteNote/", DeleteNoteView.as_view(), name="deleteNote"),  # deleteNote
-    path("saveNote/", SaveNoteView.as_view(), name="saveNote"),  # saveNote
-    path("loadNoteTree/", LoadNoteTreeView.as_view(), name="loadNoteTree"),  # loadNoteTree
-    path("newCollaborate/", NewCollaborateView.as_view(), name="newCollaborate"),  # newCollaborate
-    path("deleteCollaborate/", DeleteCollaborateView.as_view(), name="deleteCollaborate"),  # deleteCollaborate
-    path("joinCollaborate/", JoinCollaborateView.as_view(), name="joinCollaborate"),  # joinCollaborate
-    path("getTheme/", GetThemeView.as_view(), name="getTheme"),  # getTheme
-    path("deleteTheme/", DeleteThemeView.as_view(), name="deleteTheme"),  # deleteTheme
-    path('aiSocket/', AISocket.aiReturn, name='ai_return'),  # for aiSocket Http server 
-    path('api/', include('notes.urls')), # API services, including for url distribution
-    path('auth/', include('rest_framework.urls')),
-    path('api/jwtauth/', include('jwtauth.urls'), name='jwtauth'),
-    path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
-'''
+
 # Group related URL patterns
 # User note data
 note_patterns = [
